f7b22fc78c757ebe0b834cbb2fcdcdc83f5fbb08
silvaed <edsonbr27@gmail.com>
2024-08-08 20:14:13 +0100

    c4

--- galery/urls.py
+++ galery/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from galery.views import index
+from galery.views import index ,imagem
+
 
 urlpatterns = [
     path('', index),
+    path('imagem/', imagem , name='imagem'),
 ]
--- galery/views.py
+++ galery/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'galery/index.html')
+
+def imagem(request):
+    return render(request, 'galery/imagem.html')
